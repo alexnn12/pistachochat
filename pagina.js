e5a5f568aeed9b9edb27c13be0f2f08ba1026d72
alex <alexnemi@gmail.com>
2025-07-08 12:44:27 -0300

    first commit

---
+++ pagina.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { callOpenAI } = require('./components/openai');
+const dotenv = require('dotenv');
+const { askQuestion } = require('./components/cherio');
+
+dotenv.config();
+
+// Set up Express server
+const app = express();
+const PORT = process.env.PORT || 3007;
+
+//app.get('/', async (_, res) => res.send(await callOpenAI("Hello, how are you today?")));
+app.get('/', async (_, res) => res.send(await askQuestion("Quiero pagar con MercadoPago")));
+
+// Start the server
+app.listen(PORT, () => console.log(`Servidor en http://localhost:${PORT}`));
+
+// Test the OpenAI integration
+
