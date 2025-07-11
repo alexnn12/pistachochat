bca1de85f7aaee93737e78e072c12c12b6957196
alex <alexnemi@gmail.com>
2025-07-11 13:43:15 -0300

    aa

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 //app.get('/', async (_, res) => res.send(await callOpenAI("Hello, how are you today?")));
-app.get('/', async (_, res) => res.send(await askQuestion("Quiero pagar con MercadoPago")));
+app.get('/', async (_, res) => res.send(await askQuestion("Quiero pagar con tarjeta")));
 
-// Start the server
+// Start the server 
 app.listen(PORT, () => console.log(`Servidor en http://localhost:${PORT}`));
 
 // Test the OpenAI integration
