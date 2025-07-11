5dc2100953b78958ecf81d46cb405fb4f1d3ff74
alex <alexnemi@gmail.com>
2025-07-11 13:49:20 -0300

    fix api url

--- api/index.js
+++ api/index.js
@@ -11,7 +11,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 //app.get('/', async (_, res) => res.send(await callOpenAI("Hello, how are you today?")));
-app.get('/', async (_, res) => res.send(await askQuestion("Quiero pagar con tarjeta")));
+app.get('/', (_, res) => res.send('API running'));
+app.get('/api/chat', async (_, res) => res.send(await askQuestion("Quiero pagar con tarjeta")));
 
 // Start the server 
 app.listen(PORT, () => console.log(`Servidor en http://localhost:${PORT}`));
